a4090cbf9fbbbe5972966ea74dcf58c284396298
Ionică Bizău <bizauionica@yahoo.com>
2014-11-03 15:41:07 +0200

    Preparing 2.0.0

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
 // Dependencies
-var Mandrill = require("mandrill-api/mandrill");
+//var Mandrill = require("mandrill-api/mandrill");
 
 // Mandrill configuration
-var MandrillClient = new Mandrill.Mandrill(Config.mandrillConfig.key);
+//var MandrillClient = new Mandrill.Mandrill(Config.mandrillConfig.key);
+//
+//// Attach the new form
+//Bloggify.form.contact = {
+//    handler: function (req, res, formData) {
+//
+//        // TODO Antispam
+//        MandrillClient.messages.send({
+//            message: {
+//                from_email: formData.email
+//              , from_name: formData.name
+//              , to: [
+//                    {
+//                        email: Config.contact.email
+//                      , name: Config.contact.name
+//                    }
+//                ]
+//              , subject: formData.subject
+//              , text: formData.message
+//            }
+//        }, function(result) {
+//            Debug.log(result, "info");
+//            if (result.reject_reason) {
+//                return Statique.sendRes(
+//                    res, 400, "text",
+//                    JSON.stringify({ message: "Sorry, an error ocured. "
+//                      + "Try again. If the "
+//                      + "problem persists, open an issue. We log such "
+//                      + "errors, so hopefully we will fix them."
+//                    })
+//                );
+//            }
+//
+//            Statique.sendRes(
+//                res, 200, "text",
+//                JSON.stringify({ message: "Thank you for getting in touch. "
+//                   + "I will try to reply you as soon as posible."
+//                })
+//            );
+//        }, function (error) {
+//            Debug.log(error, "error");
+//            return Statique.sendRes(
+//                res, 400, "text",
+//                JSON.stringify({ message: "Sorry, an error ocured. "
+//                  + "Try again. If the "
+//                  + "problem persists, open an issue. We log such "
+//                  + "errors, so hopefully we will fix them."
+//                })
+//            );
+//        });
+//    }
+//  , validate: {
+//        email: "email"
+//      , name: "string,non-empty"
+//      , subject: "string,non-empty"
+//      , message: "string,non-empty"
+//    }
+//};
 
-// Attach the new form
-Bloggify.form.contact = {
-    handler: function (req, res, formData) {
-
-        // TODO Antispam
-        MandrillClient.messages.send({
-            message: {
-                from_email: formData.email
-              , from_name: formData.name
-              , to: [
-                    {
-                        email: Config.contact.email
-                      , name: Config.contact.name
-                    }
-                ]
-              , subject: formData.subject
-              , text: formData.message
-            }
-        }, function(result) {
-            Debug.log(result, "info");
-            if (result.reject_reason) {
-                return Statique.sendRes(
-                    res, 400, "text",
-                    JSON.stringify({ message: "Sorry, an error ocured. "
-                      + "Try again. If the "
-                      + "problem persists, open an issue. We log such "
-                      + "errors, so hopefully we will fix them."
-                    })
-                );
-            }
-
-            Statique.sendRes(
-                res, 200, "text",
-                JSON.stringify({ message: "Thank you for getting in touch. "
-                   + "I will try to reply you as soon as posible."
-                })
-            );
-        }, function (error) {
-            Debug.log(error, "error");
-            return Statique.sendRes(
-                res, 400, "text",
-                JSON.stringify({ message: "Sorry, an error ocured. "
-                  + "Try again. If the "
-                  + "problem persists, open an issue. We log such "
-                  + "errors, so hopefully we will fix them."
-                })
-            );
-        });
-    }
-  , validate: {
-        email: "email"
-      , name: "string,non-empty"
-      , subject: "string,non-empty"
-      , message: "string,non-empty"
-    }
-};
+console.log("Hello from contact form!");
